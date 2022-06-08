398b6c1f454e6e34478d243b4cc6ac042a0c59ed
saidugurlu <ugurlumsaid@gmx.de>
2022-06-08 11:45:41 +0200

    edit

--- pages/code.js
+++ pages/code.js
@@ -1,8 +1,9 @@
 export const Code = () => {
-    return (
-        <>
-            <h2>Code Page</h2>
-            <p>These are the code examples.</p>
-        </>
-    );
+  return (
+    <>
+      <h2>Code Page</h2>
+      <p>These are the code examples.</p>
+    </>
+  );
 };
+export default Code;
